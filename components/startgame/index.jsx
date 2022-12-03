bd6fb41c97cc027862d0e5ec5ab54cdf3e0bb510
Storm First <libertywalk5555@gmail.com>
2022-12-03 12:52:02 +0500

    Some images

--- components/startgame/index.jsx
+++ components/startgame/index.jsx
@@ -7,113 +7,85 @@ import circle from '../../public/images/circle.svg';
 import diamond from '../../public/images/diamond.svg';
 
 function StartGame({ question, player }) {
+  const [correct, setCorrect] = useState('');
 
+  // const hello = question.map((item) => { item.questionList[item.questionIndex].answerList.map((item2) => item2.isCorrect) });
 
-    const [correct, setCorrect] = useState("")
+  const getCorrectAnswer = (index, i) => {
+    question.map((item) => {
+      const hello = item.questionList[index].answerList.map(
+        (item2) => item2.isCorrect
+      );
 
-    // const hello = question.map((item) => { item.questionList[item.questionIndex].answerList.map((item2) => item2.isCorrect) });
+      hello[i] === true ? setCorrect('togri') : setCorrect('xato');
+    });
+  };
 
-
-    
-
-
-    const getCorrectAnswer = (index, i) => {
-
-        question.map((item) => {
-            const hello = item.questionList[index].answerList.map((item2) => item2.isCorrect);
-
-            hello[i] === true ? setCorrect("togri") : setCorrect("xato")
-
-
-
-        })
-
-
-    }
-
-
-    return (
+  return (
+    <>
+      {question.map((game) => (
         <>
+          <nav>
+            <div className="container mx-auto flex items-center justify-between border-b p-4 backdrop-blur">
+              <div className="flex lg:w-0 lg:flex-1">
+                <span className="text-sm font-bold md:text-xl">
+                  {' '}
+                  {game.questionIndex + 1} of {game.questionList.length}
+                </span>
+              </div>
+            </div>
+          </nav>
 
+          <div className="ml-2 grid gap-2  md:grid-cols-2   ">
+            {game.questionList[game.questionIndex].answerList.map((item) => (
+              <div
+                key={item.id}
+                className={
+                  item.bgColor === 'red'
+                    ? ` create-blok   bg-[#e21b3c]`
+                    : item.bgColor === 'blue'
+                    ? `create-blok  bg-[#1368ce]`
+                    : item.bgColor === 'yellow'
+                    ? `create-blok bg-[#d89e00]`
+                    : item.bgColor === 'gren'
+                    ? `create-blok bg-[#26890c]`
+                    : ''
+                }
+              >
+                <div
+                  className={`${
+                    item.svgIcon === 'diamond' ? 'rotate-45' : ''
+                  } !min-w-[30px] leading-[100%]`}
+                >
+                  <Image
+                    src={
+                      item.svgIcon === 'triangle'
+                        ? `${triangle.src}`
+                        : item.svgIcon === 'square'
+                        ? `${square.src}`
+                        : item.svgIcon === 'circle'
+                        ? `${circle.src}`
+                        : item.svgIcon === 'diamond'
+                        ? `${diamond.src}`
+                        : ''
+                    }
+                    width="60px"
+                    height="60px"
+                  />
+                </div>
 
-
-            {question.map((game) => (
-                <>
-
-                    <nav>
-                        <div className="container mx-auto flex items-center justify-between p-4 backdrop-blur border-b">
-                            <div className="flex lg:w-0 lg:flex-1">
-
-                                <span className="text-sm font-bold md:text-xl">  {game.questionIndex + 1} of{' '}
-                                    {game.questionList.length}</span>
-
-                            </div>
-
-
-                        </div>
-                    </nav>
-
-                 
-
-                        <div className="grid gap-2 md:grid-cols-2  ml-2   ">
-                            {game.questionList[
-                                game.questionIndex
-                            ].answerList.map((item) => (
-                                <div
-                                    key={item.id}
-                                    className={
-                                        item.bgColor === 'red'
-                                            ? ` bg-[#e21b3c]   create-blok`
-                                            : item.bgColor === 'blue'
-                                                ? `bg-[#1368ce]  create-blok`
-                                                : item.bgColor === 'yellow'
-                                                    ? `bg-[#d89e00] create-blok`
-                                                    : item.bgColor === 'gren'
-                                                        ? `bg-[#26890c] create-blok`
-                                                        : ''
-                                    }
-                                >
-                                    <div
-                                        className={`${item.svgIcon === 'diamond'
-                                            ? 'rotate-45'
-                                            : ''
-                                            } !min-w-[30px] leading-[100%]`}
-                                    >
-                                        <Image
-                                            src={
-                                                item.svgIcon === 'triangle'
-                                                    ? `${triangle.src}`
-                                                    : item.svgIcon === 'square'
-                                                        ? `${square.src}`
-                                                        : item.svgIcon === 'circle'
-                                                            ? `${circle.src}`
-                                                            : item.svgIcon === 'diamond'
-                                                                ? `${diamond.src}`
-                                                                : ''
-                                            }
-                                            width="60px"
-                                            height="60px"
-                                        />
-                                    </div>
-
-                                    {/* check dev */}
-
-
-                                </div>
-                            ))}
-                        </div>
-                  
-
-
-                        <footer className='w-full  mt-24 h-14 border-t flex items-center pl-10 text-lg font-bold'>
-                            {player.map((item) => item.playerName) }
-                        </footer>
-
-
-                </>
+                {/* check dev */}
+              </div>
             ))}
+          </div>
+
+          <footer className="mt-24  flex h-14 w-full items-center border-t pl-10 text-lg font-bold">
+            {player.map((item) => item.playerName)}
+          </footer>
         </>
-    );
+      ))}
+    </>
+  );
 }
 
 export default StartGame;
