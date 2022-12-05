dc180a1824721e88c0858bc8a525affa4a64156f
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2022-12-05 20:01:56 +0500

    build: add the top three with design

--- pages/game/[gameId].js
+++ pages/game/[gameId].js
@@ -2,6 +2,10 @@ import { db } from '../../firebase';
 import { collection, onSnapshot, query, where } from 'firebase/firestore';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { AiOutlineCrown } from 'react-icons/ai';
+
+import { useWindowSize } from 'react-use';
+import Confetti from 'react-confetti';
 
 import { StartGame, WaitingRoom } from 'components';
 
@@ -14,6 +18,8 @@ function GameId() {
   const [showCount, setShowCount] = useState(false);
   const router = useRouter();
   const { gameId } = router.query;
+
+  const { width, height } = useWindowSize();
 
   useEffect(() => {
     question.map((item) =>
@@ -158,42 +164,72 @@ function GameId() {
                       </div>
                     )
                   ) : item.status === 'result' ? (
-                    <div className="flex h-[100vh] w-full flex-col items-center justify-center">
-                      {/* <div className="flex justify-around w-[40%] items-end">
-                    <div className="w-48 h-72 bg-yellow-500 rounded-lg">
-                      <p className="text-center text-white font-semibold p-4 text-2xl">
-                      </p>
-                    </div>
-                    <div className="w-48 h-96 bg-blue-500 rounded-lg">
-                      <p className="text-center text-white font-semibold p-4 text-2xl">
-                      </p>
-                    </div>
-                    <div className="w-48 h-60 bg-rose-500 rounded-lg">
-                      <p className="text-center text-white font-semibold p-4 text-2xl">
-                      </p>
-                    </div>
-                  </div> */}
-                      <div className="mt-14 flex w-full justify-center">
-                        <table className="w-[50%] border">
-                          <tr className="cursor-pointer border-[1px] hover:bg-gray-100 active:bg-sky-200">
-                            <th>№</th>
-                            <th>Ismi</th>
-                            <th>Ball</th>
-                            <th>To`g`ri javoblar</th>
-                            <th>Noto`g`ri javoblar</th>
-                            <th>Savollar soni</th>
-                          </tr>
-                          <tr className="cursor-pointer border-[1px] hover:bg-gray-100 active:bg-sky-200">
-                            <td>{index + 1}</td>
-                            <td>{item0.playerName}</td>
-                            <td>{item0.point}</td>
-                            <td>{item0.intPoint}</td>
-                            <td>
-                              {item.questionList.length - item0.intPoint}
-                            </td>
-                            <td>{item.questionList.length}</td>
-                          </tr>
-                        </table>
+                    <div className="flex h-screen flex-col justify-center">
+                      <div className="hidden lg:block">
+                        <Confetti
+                          width={width}
+                          height={height}
+                          tweenDuration={5000}
+                        />
+                      </div>
+                      <div className="flex items-end justify-center space-x-2">
+                        <div className="relative flex h-24 w-20 items-center justify-center rounded-t-xl rounded-b-lg bg-[#F9C200] hover:bg-opacity-80 md:h-36 md:w-40 lg:h-40 lg:w-48">
+                          <div className="absolute top-0 left-0 flex h-10 w-full items-center justify-center rounded-t-xl bg-[#F99500]">
+                            <h3 className="font-semibold text-white md:text-lg">
+                              Messi
+                            </h3>
+                          </div>
+                          <h3 className="text-3xl font-bold text-white md:text-5xl">
+                            3
+                          </h3>
+                        </div>
+                        <div className="relative flex h-36 w-20 items-center justify-center rounded-lg bg-[#006ED4] hover:bg-opacity-80 md:h-56 md:w-40 lg:h-72 lg:w-48">
+                          <div className="absolute -top-7 left-1/2 -translate-x-1/2 transform text-2xl text-[#006ED4] md:-top-12 md:text-5xl">
+                            <AiOutlineCrown />
+                          </div>
+                          <div className="absolute top-0 left-0 flex h-10 w-full items-center justify-center rounded-t-xl bg-[#0060B8]">
+                            <h3 className="font-semibold text-white md:text-lg">
+                              Ronaldo
+                            </h3>
+                          </div>
+                          <h3 className="text-3xl font-bold text-white md:text-5xl">
+                            1
+                          </h3>
+                        </div>
+                        <div className="relative flex h-28 w-20 items-center justify-center rounded-lg bg-[#EC5858] hover:bg-opacity-80 md:h-44 md:w-40 lg:h-56 lg:w-48">
+                          <div className="absolute top-0 left-0 flex h-10 w-full items-center justify-center rounded-t-xl bg-[#D93C3C]">
+                            <h3 className="font-semibold text-white md:text-lg">
+                              Mbabpe
+                            </h3>
+                          </div>
+                          <h3 className="text-3xl font-bold text-white md:text-5xl">
+                            2
+                          </h3>
+                        </div>
+                      </div>
+                      <div>
+                        <div className="mt-14 flex w-full justify-center">
+                          <table className="w-[50%] border">
+                            <tr className="cursor-pointer border-[1px] hover:bg-gray-100 active:bg-sky-200">
+                              <th>№</th>
+                              <th>Ismi</th>
+                              <th>Ball</th>
+                              <th>To`g`ri javoblar</th>
+                              <th>Noto`g`ri javoblar</th>
+                              <th>Savollar soni</th>
+                            </tr>
+                            <tr className="cursor-pointer border-[1px] hover:bg-gray-100 active:bg-sky-200">
+                              <td>{index + 1}</td>
+                              <td>{item0.playerName}</td>
+                              <td>{item0.point}</td>
+                              <td>{item0.intPoint}</td>
+                              <td>
+                                {item.questionList.length - item0.intPoint}
+                              </td>
+                              <td>{item.questionList.length}</td>
+                            </tr>
+                          </table>
+                        </div>
                       </div>
                     </div>
                   ) : (
